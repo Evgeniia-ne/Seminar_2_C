956cc36b75273efb4f18472add6e13a906d59dca
Asus <koncarik@mail.ru>
2022-12-01 20:23:02 +0500

    Commit 1

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+int DataЕntryXY(string str)
+{
+    Console.Write(str);
+     int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void FindKvandrat(int x, int y)
+{
+    if ((x>0) && (y>0)) Console.WriteLine("Это первый квадрант");
+    if ((x<0) && (y>0)) Console.WriteLine("Это второй квадрант");
+    if ((x<0) && (y<0)) Console.WriteLine("Это четвертый квадрант");
+    if ((x>0) && (y<0)) Console.WriteLine("Это третий квадрант");
+}
+
+int x = DataЕntryXY("Введите координаты x: ");
+//Console.WriteLine(x);
+int y = DataЕntryXY("Введите координаты y: ");
+//Console.WriteLine(y);
+FindKvandrat (x,y);

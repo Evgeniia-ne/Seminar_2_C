bfcbde3da10cb96cd96d9d6ac5b7ada683cd5f3d
Asus <koncarik@mail.ru>
2022-12-01 20:41:59 +0500

    Commit 2

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+Console.Clear();
+int DataЕntry(string str)
+{
+    Console.Write(str);
+     int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void FindDiapazon(int kvadrant)
+{
+    if (kvadrant == 1) Console.WriteLine("(x>0) и (y>0)");
+    if (kvadrant == 2) Console.WriteLine("(x<0) и (y>0)");
+    if (kvadrant == 3) Console.WriteLine("(x<0) и (y<0)");
+    if (kvadrant == 4) Console.WriteLine("(x>0) и (y<0)");
+}
+
+int kvadrant = DataЕntry("Введите номер квадранта: ");
+
+FindDiapazon (kvadrant);
